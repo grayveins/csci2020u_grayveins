8d461a54c84464589615ae472663e8fbec3b18bb
grayveins <trohoy123@gmail.com>
2022-01-25 13:11:03 -0500

    last commit

--- lab01/example.java
+++ lab01/example.java
@@ -1,5 +1,7 @@
 public class example {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        System.out.println("This was changed on branch beta.");
+        System.out.println("This was changed on main.");
     }
 }
